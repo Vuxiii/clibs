d487df7607ff309eea6d0a7e66b0c7a4f031252d
William <william.kramhyle@gmail.com>
2023-12-29 00:08:38 +0100

    Migrate to using string interpolation when printing -> Do a single syscall print. Implemented some horrible default float and double formatters. They work, but are not pretty. Also made default i32, u32, i64, u64 formatters.

--- library.c
+++ library.c
@@ -21,48 +21,65 @@ int double_less( const void *a, const void *b ) {
 
 int main(void) {
 
-    ArrayList arr = arraylist_new(sizeof(i32));
-    array_push(&arr, -1);
-    array_push(&arr, 9);
-    array_push(&arr, -2);
-    array_push(&arr, 4);
-    array_push(&arr, -8);
+    const Str s = str_format("My number is {i32}\n", 42);
 
-    forward_it(arr, i32) {
-        print("{i32}\n", *it);
-    }
-    arraylist_sort(arr, int_less);
-    forward_it(arr, i32) {
-        print("{i32}\n", *it);
-    }
+//    print(s);
+//
+//    print("{u64}\n", -1);
+//    print("{u32}\n", -1);
+//    print("{i32}\n", 0);
+//    print("{i32}\n", -0);
+//    print("{u32}\n", 10);
+//    print("{u32}\n", 0);
+//    print("{u32}\n", -0);
+    print("{f32}\n", 7.0045000301);
 
-    str_register( "{Point}", Point_Printer);
-
-    ArrayList points = arraylist_new(sizeof(Point));
-    arraylist_push(&points, &(Point){1.0, 2.0});
-    arraylist_push(&points, &(Point){3.0, 4.0});
-    arraylist_push(&points, &(Point){5.0, 6.0});
-
-    forward_it(points, Point) {
-        print("{Point}\n", *it);
-    }
-
-    Point *last_point = &arraylist_at(points, 2, Point);
-
-    print("Last point: {Point}\n", *last_point);
-
-    reverse_it(points, Point) {
-        print("{Point}\n", *it);
-    }
-
-    for ( Point *it = points.data; it <= &arraylist_last(points, Point); ++it ) {
-        print("Point {Point}\n", *it);
-    }
-
-    print("Hello {Point}\n", arraylist_at(points, 1, Point));
-
-//    Str s;
-    print(str_from_cstr("Hello {Point}\n"), (Point){1.0, 2.0});
+//    ArrayList arr = arraylist_new(sizeof(i32));
+//    array_push(&arr, -1);
+//    array_push(&arr, 9);
+//    array_push(&arr, -2);
+//    array_push(&arr, 4);
+//    array_push(&arr, -8);
+//
+//    forward_it(arr, i32) {
+//        print("{i32}\n", *it);
+//    }
+//    arraylist_sort(arr, int_less);
+//    forward_it(arr, i32) {
+//        print("{i32}\n", *it);
+//    }
+//
+//    str_register( "{Point}", Point_Printer);
+//
+//    ArrayList points = arraylist_new(sizeof(Point));
+//    arraylist_push(&points, &(Point){1.0, 2.0});
+//    arraylist_push(&points, &(Point){3.0, 4.0});
+//    arraylist_push(&points, &(Point){5.0, 6.0});
+//
+//    forward_it(points, Point) {
+//        print("{Point}\n", *it);
+//    }
+//
+//    Point *last_point = &arraylist_at(points, 2, Point);
+//
+//    print("Last point: {Point}\n", *last_point);
+//
+//    reverse_it(points, Point) {
+//        print("{Point}\n", *it);
+//    }
+//
+//    for ( Point *it = points.data; it <= &arraylist_last(points, Point); ++it ) {
+//        print("Point {Point}\n", *it);
+//    }
+//
+//    print("Hello {Point}\n", arraylist_at(points, 1, Point));
+//
+//    arraylist_free(&points);
+//
+//    print("Hello {Point}\n", (Point){7.4, 2.0});
+//
+////    Str s;
+//    print(str_from_cstr("Hello {Point}\n"), (Point){1.0, 2.0});
 //    print("Hello {Point}\n", (Point){1.0, 2.0});
 
 //    bool a = false;
--- library.h
+++ library.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stddef.h>
-
+#include <unistd.h>
 
 typedef unsigned int u32;
 typedef int i32;
@@ -69,7 +69,7 @@ typedef struct Str {
 
 typedef struct FormatOption {
     Str format;
-    void (* _Nonnull printer)(va_list * _Nonnull args);
+    const Str (* _Nonnull printer)(va_list * _Nonnull args);
 } FormatOption;
 #define MAX_FORMAT_OPTIONS 20
 
@@ -81,7 +81,10 @@ const Str str_concat(const Str a, const Str b);
 const Str str_from_cstr(const char * _Nonnull cstr);
 bool str_a_contains_b(const Str a, const Str b);
 
-void str_register(const char * _Nonnull format, void (* _Nonnull printer)(va_list * _Nonnull args));
+const Str __attribute__((overloadable)) str_format(char * _Nonnull format, ...);
+const Str __attribute__((overloadable)) str_format(const Str format, ...);
+
+void str_register(const char * _Nonnull format, const Str (* _Nonnull printer)(va_list * _Nonnull args));
 
 #ifndef LIBS_IMPLEMENTATION
 #define LIBS_IMPLEMENTATION
@@ -129,75 +132,309 @@ void arraylist_remove(ArrayList * _Nonnull list, u32 index) {
 
 static u32 _num_options = 0;
 static FormatOption options[MAX_FORMAT_OPTIONS] = {0};
-void str_register(const char * _Nonnull format, void (* _Nonnull printer)(va_list * _Nonnull args)) {
+void str_register(const char * _Nonnull format, const Str (* _Nonnull printer)(va_list * _Nonnull args)) {
     assert(_num_options < MAX_FORMAT_OPTIONS); // Ensure that enough space has been allocated for the format options.
     options[_num_options].format = str_from_cstr(format);
     options[_num_options].printer = printer;
     _num_options++;
 }
 
-void i32_Printer(va_list * _Nonnull args) {
-    i32 i = va_arg(*args, i32);
-    printf("%d", i);
+static const Str i32_Printer(va_list * _Nonnull args) {
+    i32 number = va_arg(*args, i32);
+    u32 len = 0;
+    i32 temp = number;
+    if (number == 0) {
+        return str_from_cstr("0");
+    }
+    if (temp < 0) {
+        len++;
+        temp = -temp;
+    }
+    while (temp > 0) {
+        len++;
+        temp /= 10;
+    }
+    char *str = malloc(len + 1);
+    str[len] = '\0';
+    temp = number;
+    if (temp < 0) {
+        str[0] = '-';
+        temp = -temp;
+    }
+    for (u32 i = len - 1; i > 0; i--) {
+        str[i] = (temp % 10) + '0';
+        temp /= 10;
+    }
+    if (number >= 0) {
+        str[0] = (temp % 10) + '0';
+    }
+    return (Str){str, len};
 }
 
-void u32_Printer(va_list * _Nonnull args) {
-    u32 i = va_arg(*args, u32);
-    printf("%u", i);
+static const Str u32_Printer(va_list * _Nonnull args) {
+    u32 number = va_arg(*args, u32);
+    if (number == 0) {
+        char *str = malloc(2);
+        str[0] = '0';
+        str[1] = '\0';
+        return (Str){str, 1};
+    }
+    u32 len = 0;
+
+    u32 temp = number;
+    while (temp > 0) {
+        len++;
+        temp /= 10;
+    }
+    char *str = malloc(len + 1);
+    str[len+1] = '\0';
+    temp = number;
+    for (i32 i = len - 1; i >= 0; i--) {
+        str[i] = (temp % 10) + '0';
+        temp /= 10;
+    }
+    return (Str){str, len};
 }
 
-void i64_Printer(va_list * _Nonnull args) {
-    i64 i = va_arg(*args, i64);
-    printf("%lld", i);
+static const Str i64_Printer(va_list * _Nonnull args) {
+    i64 number = va_arg(*args, i64);
+    u32 len = 0;
+    i64 temp = number;
+    if (number == 0) {
+        return str_from_cstr("0");
+    }
+    if (temp < 0) {
+        len++;
+        temp = -temp;
+    }
+    while (temp > 0) {
+        len++;
+        temp /= 10;
+    }
+    char *str = malloc(len + 1);
+    str[len] = '\0';
+    temp = number;
+    if (temp < 0) {
+        str[0] = '-';
+        temp = -temp;
+    }
+    for (u32 i = len - 1; i > 0; i--) {
+        str[i] = (temp % 10) + '0';
+        temp /= 10;
+    }
+    if (number >= 0) {
+        str[0] = (temp % 10) + '0';
+    }
+    return (Str){str, len};
 }
 
-void u64_Printer(va_list * _Nonnull args) {
-    u64 i = va_arg(*args, u64);
-    printf("%llu", i);
+static const Str u64_Printer(va_list * _Nonnull args) {
+    u64 number = va_arg(*args, u64);
+    if (number == 0) {
+        char *str = malloc(2);
+        str[0] = '0';
+        str[1] = '\0';
+        return (Str){str, 1};
+    }
+    u32 len = 0;
+
+    u64 temp = number;
+    while (temp > 0) {
+        len++;
+        temp /= 10;
+    }
+    char *str = malloc(len + 1);
+    str[len+1] = '\0';
+    temp = number;
+    for (i32 i = len - 1; i >= 0; i--) {
+        str[i] = (temp % 10) + '0';
+        temp /= 10;
+    }
+    return (Str){str, len};
 }
 
-void f32_Printer(va_list * _Nonnull args) {
-    f32 i = va_arg(*args, f64);
-    printf("%f", i);
+static const Str f32_Printer(va_list * _Nonnull args) {
+    f64 number = va_arg(*args, f64);
+
+    i32 int_part = (i32)number;
+    f32 frac_part = (number - int_part);
+    i32 pad_with_zero = -1;
+    u32 frac_len = 0;
+    while (frac_part != (i32)frac_part) {
+        if ((i32)frac_part == 0) {
+            pad_with_zero++;
+        } else {
+            ++frac_len;
+        }
+        frac_part *= 10;
+    }
+    i32 frac_part_i32 = (i32)frac_part;
+
+    Str int_str;
+    {
+        u32 len = 0;
+        i64 temp = int_part;
+        if (temp == 0) {
+            int_str = str_from_cstr("0");
+        } else {
+            if (temp < 0) {
+                len++;
+                temp = -temp;
+            }
+            while (temp > 0) {
+                len++;
+                temp /= 10;
+            }
+            char *str = malloc(len + 1);
+            str[len] = '\0';
+            temp = int_part;
+            if (temp < 0) {
+                str[0] = '-';
+                temp = -temp;
+            }
+            for (u32 i = len - 1; i > 0; i--) {
+                str[i] = (temp % 10) + '0';
+                temp /= 10;
+            }
+            if (temp >= 0) {
+                str[0] = (temp % 10) + '0';
+            }
+            int_str = (Str) {str, len};
+        }
+    }
+    Str frac_str;
+    {
+        u32 len = 1 + frac_len;
+        i64 temp = frac_part_i32;
+        if (number == 0) {
+            frac_str = str_from_cstr("0");
+        } else {
+            if (temp < 0) {
+                temp = -temp;
+            }
+            len += pad_with_zero;
+            char *str = malloc(len + 1);
+            str[len] = '\0';
+            temp = frac_part_i32;
+            if (temp < 0) {
+                temp = -temp;
+            }
+            for (u32 i = len - 1; i > 0; i--) {
+                str[i] = (temp % 10) + '0';
+                temp /= 10;
+            }
+            if (temp >= 0) {
+                str[0] = (temp % 10) + '0';
+            }
+            for (u32 i = 0; i < pad_with_zero; i++) {
+                str[i] = '0';
+            }
+            frac_str = (Str) {str, len};
+        }
+    }
+
+    const Str dot_str = str_from_cstr(".");
+    const Str out = str_concat(int_str, str_concat(dot_str, frac_str));
+    return out;
 }
 
-void f64_Printer(va_list * _Nonnull args) {
-    f64 i = va_arg(*args, f64);
-    printf("%f", i);
+static const Str f64_Printer(va_list * _Nonnull args) {
+    f64 number = va_arg(*args, f64);
+
+    i64 int_part = (i64)number;
+    f64 frac_part = (number - int_part);
+    i32 pad_with_zero = -1;
+    u32 frac_len = 0;
+    while (frac_part != (i64)frac_part) {
+        if ((i64)frac_part == 0) {
+            pad_with_zero++;
+        } else {
+            ++frac_len;
+        }
+        frac_part *= 10;
+    }
+    i64 frac_part_i32 = (i64)frac_part;
+
+    Str int_str;
+    {
+        u32 len = 0;
+        i64 temp = int_part;
+        if (temp == 0) {
+            int_str = str_from_cstr("0");
+        } else {
+            if (temp < 0) {
+                len++;
+                temp = -temp;
+            }
+            while (temp > 0) {
+                len++;
+                temp /= 10;
+            }
+            char *str = malloc(len + 1);
+            str[len] = '\0';
+            temp = int_part;
+            if (temp < 0) {
+                str[0] = '-';
+                temp = -temp;
+            }
+            for (u32 i = len - 1; i > 0; i--) {
+                str[i] = (temp % 10) + '0';
+                temp /= 10;
+            }
+            if (temp >= 0) {
+                str[0] = (temp % 10) + '0';
+            }
+            int_str = (Str) {str, len};
+        }
+    }
+    Str frac_str;
+    {
+        u32 len = 1 + frac_len;
+        i64 temp = frac_part_i32;
+        if (number == 0) {
+            frac_str = str_from_cstr("0");
+        } else {
+            if (temp < 0) {
+                temp = -temp;
+            }
+            len += pad_with_zero;
+            char *str = malloc(len + 1);
+            str[len] = '\0';
+            temp = frac_part_i32;
+            if (temp < 0) {
+                temp = -temp;
+            }
+            for (u32 i = len - 1; i > 0; i--) {
+                str[i] = (temp % 10) + '0';
+                temp /= 10;
+            }
+            if (temp >= 0) {
+                str[0] = (temp % 10) + '0';
+            }
+            for (u32 i = 0; i < pad_with_zero; i++) {
+                str[i] = '0';
+            }
+            frac_str = (Str) {str, len};
+        }
+    }
+
+    const Str dot_str = str_from_cstr(".");
+    const Str out = str_concat(int_str, str_concat(dot_str, frac_str));
+    return out;
 }
 
-void u8_Printer(va_list * _Nonnull args) {
-    u8 i = va_arg(*args, u32);
-    printf("%u", i);
-}
-
-void i8_Printer(va_list * _Nonnull args) {
-    i8 i = va_arg(*args, i32);
-    printf("%d", i);
-}
-
-void u16_Printer(va_list * _Nonnull args) {
-    u16 i = va_arg(*args, u32);
-    printf("%u", i);
-}
-
-void i16_Printer(va_list * _Nonnull args) {
-    i16 i = va_arg(*args, i32);
-    printf("%d", i);
-}
-
-void bool_Printer(va_list * _Nonnull args) {
+static const Str bool_Printer(va_list * _Nonnull args) {
     bool i = va_arg(*args, bool);
     if (i) {
-        printf("true");
+        return str_from_cstr("true");
     } else {
-        printf("false");
+        return str_from_cstr("false");
     }
 }
 
-void str_Printer(va_list * _Nonnull args) {
+static const Str str_Printer(va_list * _Nonnull args) {
     Str str = va_arg(*args, Str);
-    printf("%.*s", str.len, str.str);
+    return str;
 }
 
 #if defined(__clang__)
@@ -208,28 +445,31 @@ static void init_printers(void) __attribute__((constructor)) {
     str_register("{u64}", u64_Printer);
     str_register("{f32}", f32_Printer);
     str_register("{f64}", f64_Printer);
-    str_register("{u8}", u8_Printer);
-    str_register("{i8}", i8_Printer);
-    str_register("{u16}", u16_Printer);
-    str_register("{i16}", i16_Printer);
     str_register("{bool}", bool_Printer);
     str_register("{str}", str_Printer);
 }
 #endif
 
-static inline void print_impl(const Str format, va_list args) {
+static inline const Str str_format_impl(const Str format, va_list args ) {
+    ArrayList strs = arraylist_new(sizeof(Str));
     u32 last_printed = 0;
     // Scan through the format string and discover any registered format options.
     for (u32 i = 0; i < format.len; i++) {
         if (format.str[i] == '\\' && format.str[i + 1] == '{') {
             // We want to print upto here, but not the next character.
-            printf("%.*s{", i - last_printed, format.str + last_printed);
+            arraylist_push(&strs, &(Str){
+                    .str = format.str + last_printed,
+                    .len = i - last_printed
+            });
             i++;
             last_printed = i + 1;
             continue;
         }
         if (format.str[i] == '{') {
-            printf("%.*s", i - last_printed, format.str + last_printed);
+            arraylist_push(&strs, &(Str){
+                    .str = format.str + last_printed,
+                    .len = i - last_printed
+            });
             u32 j = i;
             bool found = false;
             while (j < format.len) {
@@ -246,7 +486,8 @@ static inline void print_impl(const Str format, va_list args) {
                 found = false;
                 for ( u32 k = 0; k < _num_options; k++ ) {
                     if ( str_eq( option, options[k].format ) ) {
-                        options[k].printer(&args);
+                        const Str fmt = options[k].printer(&args);
+                        arraylist_push(&strs, &fmt);
                         i = j + 1;
                         found = true;
                         break;
@@ -259,21 +500,59 @@ static inline void print_impl(const Str format, va_list args) {
             }
         }
     }
-    printf("%.*s", format.len - last_printed, format.str + last_printed);
+    arraylist_push(&strs, &(Str){
+            .str = format.str + last_printed,
+            .len = format.len - last_printed
+    });
+    u32 total_len = 0;
+    forward_it(strs, Str) {
+        total_len += it->len;
+    }
+    char *str = malloc(total_len + 1);
+    u32 offset = 0;
+    forward_it(strs, Str) {
+        for (u32 i = 0; i < it->len; i++) {
+            str[offset + i] = it->str[i];
+        }
+        offset += it->len;
+    }
+    str[total_len] = '\0';
+
+    arraylist_free(&strs);
+    return (Str){str, total_len};
+}
+
+const Str __attribute__((overloadable)) str_format(char * _Nonnull format_c, ...) {
+    va_list args;
+    va_start(args, format_c);
+    const Str format = str_from_cstr(format_c);
+    const Str out = str_format_impl(format, args);
+    va_end(args);
+    return out;
+}
+
+const Str __attribute__((overloadable)) str_format(const Str format, ...) {
+    va_list args;
+    va_start(args, format);
+    const Str out = str_format_impl(format, args);
+    va_end(args);
+    return out;
 }
 
 void __attribute__((overloadable)) print(char * _Nonnull format_c, ...) {
     va_list args;
     va_start(args, format_c);
-    const Str format = str_from_cstr(format_c);
-    print_impl(format, args);
+    const Str string = str_format_impl(str_from_cstr(format_c), args);
+
+    write(STDOUT_FILENO, string.str, string.len);
     va_end(args);
 }
 
 void __attribute__((overloadable)) print(const Str format, ...) {
     va_list args;
     va_start(args, format);
-    print_impl(format, args);
+    const Str string = str_format_impl(format, args);
+    write(STDOUT_FILENO, string.str, string.len);
     va_end(args);
 }
 
